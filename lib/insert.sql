b527799969cb31740f76b445116b6769ff87720f
Julie C <jucheng925@gmail.com>
2023-09-29 13:55:20 -0400

    practice sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "black", "always hungry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1, "F", "brown", "loves parties", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "F", "brown and white", "sleep all day", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "dark brown", "proud and stotic", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 7, "F", "black", "neat and proper", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "M", "green", "not social", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, "F", "brown", "laugh all day", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, "M", "black", NULL, 0);
